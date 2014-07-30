7d1e8291c51abdd846642c5dfe6a097edb151d36
Alexandre JARNOUX <alex.jarnoux@gmail.com>
2014-07-30 10:39:20 +0200

    Fix CRLF

--- src/main/scala/ReadFromES.scala
+++ src/main/scala/ReadFromES.scala
@@ -2,6 +2,7 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.io.{MapWritable, Text}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.serializer.KryoSerializer
+import org.elasticsearch.hadoop.cfg.ConfigurationOptions
 import org.elasticsearch.hadoop.mr.EsInputFormat
 
 object ReadFromES {
@@ -10,10 +11,10 @@ object ReadFromES {
     val sc = new SparkContext(conf)
     sc.setLocalProperty("spark.serializer", classOf[KryoSerializer].getName)
     val esconf = new Configuration()
-    esconf.set("es.nodes", "vps67962.ovh.net")
-    esconf.set("es.port", "9200")
-    esconf.set("es.resource", "fta/customer") // index/type
-    esconf.set("es.query", "?q=alex")
+    esconf.set(ConfigurationOptions.ES_NODES, "vps67962.ovh.net")
+    esconf.set(ConfigurationOptions.ES_PORT, "9200")
+    esconf.set(ConfigurationOptions.ES_RESOURCE, "fta/customer") // index/type
+    esconf.set(ConfigurationOptions.ES_QUERY, "?q=alex")
     val esRDD = sc.newAPIHadoopRDD(esconf, classOf[EsInputFormat[Text, MapWritable]], classOf[Text], classOf[MapWritable])
 
     val docCount = esRDD.count()
