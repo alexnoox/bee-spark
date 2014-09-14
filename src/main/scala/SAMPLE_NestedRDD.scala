b98b4a9c9af62dc84614719793aba5704729cbf5
robbee <alexandre.bonte@beesearch.fr>
2014-09-14 08:49:09 +0200

    nested document map/reduce with contact

--- src/main/scala/SAMPLE_NestedRDD.scala
+++ src/main/scala/SAMPLE_NestedRDD.scala
@@ -17,11 +17,15 @@ object SAMPLE_NestedRDD {
     val format = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
 
     case class Customer (customerId: Int, name: String)
+    case class Contact (contactId: Int, customerId: Int, firstName: String,  lastName: String, email: String, tel: String, avatar: String)
     case class Order (orderId: Int, customerId: Int, orderName: String)
     case class OrderLine (orderLineId: Int, orderId: Int, lineAmount: Int )
 
     val customer = sc.textFile(getClass.getResource("fake-customer-qn.csv").toString).map(_.split(";")).map(
       c => (c(0).toInt, Customer(c(0).toInt, c(1) ))
+    )
+    val contact = sc.textFile(getClass.getResource("fake-contact-qn.csv").toString).map(_.split(";")).map(
+      c => (c(1).toInt, Contact(c(0).toInt, c(1).toInt, c(2), c(3), c(4), c(5), c(6) ))
     )
     val order = sc.textFile(getClass.getResource("fake-order-qn.csv").toString).map(_.split(";")).map(
       o => (o(0).toInt, Order(o(0).toInt, o(1).toInt, o(2) ))
@@ -30,45 +34,45 @@ object SAMPLE_NestedRDD {
       ol => (ol(1).toInt, OrderLine(ol(0).toInt, ol(1).toInt, ol(3).toInt))
     )
 
-    case class mainCustomer (customerId: Int, name: String, order: mainOrder)
-    case class mainOrder (orderId: Int, customerId: Int, orderName: String, orderLine: mainOrderLine)
-    case class mainOrderLine (orderLineId: Int, orderId: Int, lineAmount: Int )
 
-
+    /*
     val x = List("a" -> "b", "c" -> "d", "a" -> "f")
-    //x: List[(java.lang.String, java.lang.String)] = List((a,b), (c,d), (a,f))
     val x1 = x.groupBy(_._1)
+    //(a,List((a,b), (a,f)))
+    //(c,List((c,d)))
     val x2 = x.groupBy(_._1).map { case (k,v) => (k,v.map(_._2))}
-    println("--------")
-    x1.foreach(println)
-    println("--------")
-    x2.foreach(println)
+    //(a,List(b, f))
+    //(c,List(d))
+    */
 
-    println("--------")
-    /*val tuple_o_ol = order.cogroup(orderLine).map(x => (x._2._1.head.customerId, (
-      x._2._1.head.orderId,
-      x._2._1.head.customerId,
-      x._2._1.head.orderName,
-      x._2._2.toList
-    )))*/
-
-    val tuple_o_ol = order.cogroup(orderLine).map { case (k,v) => (v._1.head.customerId,(v._1,v._2)) }
-    tuple_o_ol.foreach(println)
 
     println("--------")
-    val tuple = customer.cogroup(tuple_o_ol)
-    //val tuple = customer.cogroup(order.cogroup(orderLine))
-    tuple.foreach(println)
-
-
+    val tuple = customer.cogroup(
+      order.cogroup(orderLine).map { case (k,v) => (v._1.head.customerId,(v._1,v._2)) }
+    ).cogroup(contact)
 
     val r1 = tuple.map((t) => {
       val custBson = new BasicBSONObject()
       val orderBsonList = new BasicBSONList()
-      custBson.put("customerId", t._2._1.head.customerId)
-      custBson.put("name", t._2._1.head.name)
+      val customerBsonList = new BasicBSONList()
 
-      t._2._2.foreach { o =>
+      custBson.put("customerId", t._2._1.head._1.head.customerId)
+      custBson.put("name", t._2._1.head._1.head.name)
+
+      t._2._2.foreach { (c: Contact) =>
+        val contactBson = new BasicBSONObject()
+        contactBson.put("id", c.contactId)
+        contactBson.put("customerId", c.customerId)
+        contactBson.put("firstName", c.firstName)
+        contactBson.put("lastName", c.lastName)
+        contactBson.put("email", c.email)
+        contactBson.put("phone", c.tel)
+        contactBson.put("avatar", c.avatar)
+        customerBsonList.add(contactBson)
+      }
+      custBson.put("contacts", customerBsonList)
+
+      t._2._1.head._2.foreach { o =>
         val orderBson = new BasicBSONObject()
         val orderLineBsonList = new BasicBSONList()
         orderBson.put("id", o._1.head.orderId)
@@ -91,8 +95,6 @@ object SAMPLE_NestedRDD {
     })
 
     r1.saveAsNewAPIHadoopFile("file:///bogus", classOf[Any], classOf[Any], classOf[com.mongodb.hadoop.MongoOutputFormat[Any, Any]], mongoCustomerConf)
-
-
 
   }
 
